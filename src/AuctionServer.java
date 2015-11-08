b61bbe598b30cf400fe2eb918378560d1e992043
Velin Kerkov <v.b.k.2007@abv.bg>
2015-11-08 21:10:23 +0000

    AuctionItem command line argument to load from file

--- src/AuctionServer.java
+++ src/AuctionServer.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by vbk20 on 29/10/2015.
@@ -10,29 +12,33 @@ public class AuctionServer {
     static final long ONE_MINUTE_IN_MILLIS = 60000;//millisecs
     private Auction auction; // for direct access
     private long id;
+    private Logger logger;
+    private static final Level DEFAULT_LOG_LEVEL = Level.INFO;
 
-    public AuctionServer(String auctionName) {
+    public AuctionServer(String[] args) {
+        logger = Logger.getLogger(this.getClass().getName());
+        String auctionName = args[0];
         try {
-            System.out.println("Creating Auction.");
+            logger.log(DEFAULT_LOG_LEVEL, "Creating Auction (-- {0} --)", auctionName);
             auction = new Auction(auctionName);
             this.id = auction.getServerId("SERVER_ID_TOKEN");
 
             IAuctionRemote auctionRemote = auction; // for Remote Registering
-            System.out.format("Auction created (%s)\n. Now lets register it.\n", auction);
+            logger.log(DEFAULT_LOG_LEVEL, "Auction created \n. Now lets register it.\n");
+
             Registry auctionServerRegistry = LocateRegistry.getRegistry("localhost", Utils.AUCTION_SERVER_RMI_PORT);
             auctionServerRegistry.rebind(Utils.AUCTION_REGISTRY_NAME, auctionRemote);
-            System.out.println("Auction registered to the global AUCTION_SERVER RMI registry.\n");
+            logger.log(DEFAULT_LOG_LEVEL, "Auction registered to the global AUCTION_SERVER RMI registry.\n");
 
-//            System.out.println("Adding initial bids.\n");
-//            Date now = new Date();
-//            Date defaultEndDate = new Date(now.getTime() + 10 * ONE_MINUTE_IN_MILLIS);
-//            auction.createAndRegisterAuctionItem(Utils.DEFAULT_CREATOR_ID, "Guitar", 100, defaultEndDate);
-//            auction.createAndRegisterAuctionItem(Utils.DEFAULT_CREATOR_ID, "Piano", 100, defaultEndDate);
-//            auction.createAndRegisterAuctionItem(Utils.DEFAULT_CREATOR_ID, "Bottle", 100, defaultEndDate);
+            try {
+                String fileName = args[1];
+                readAuctionItemsFromFile(fileName);
+            } catch (IndexOutOfBoundsException e) {
+                logger.log(DEFAULT_LOG_LEVEL, "No file supplied to pre-load AuctionItems!\n");
+            }
 
-            System.out.println("---------------------------- AUCTION STARTED -----------------------------\n");
+            logger.log(DEFAULT_LOG_LEVEL, "---------------------------- AUCTION STARTED -----------------------------\n");
         } catch (Exception e) {
-            System.out.format("export exception - %s\n", e.getMessage());
             e.printStackTrace();
             System.exit(1);
         }
@@ -53,12 +59,11 @@ public class AuctionServer {
                                 case 2:
                                     String fileName = input[1];
                                     File f = new File(fileName);
-                                    if(!f.exists()){
+                                    if (!f.exists()) {
                                         f.createNewFile();
                                     }
                                     try (FileWriter fr = new FileWriter(f); BufferedWriter bfr = new BufferedWriter(fr)) {
                                         List<Object[]> liveActionItemsForStorage = auction.getLiveActionItemsForStorage(this.id);
-                                        System.out.println("AuctionLiveItems: "+ liveActionItemsForStorage.size());
                                         StringBuilder b = new StringBuilder();
                                         for (Object[] itemProperties : liveActionItemsForStorage) {
                                             for (Object property : itemProperties) {
@@ -67,17 +72,17 @@ public class AuctionServer {
                                             b.append("\n");
                                         }
                                         bfr.write(b.toString());
-                                        System.out.format("Live AuctionItems exported to file '%s'.\n", fileName);
+                                        logger.log(DEFAULT_LOG_LEVEL, "{0} Live AuctionItems exported to file '{1}'.\n", new Object[]{liveActionItemsForStorage.size(), fileName});
                                     } catch (FileNotFoundException e) {
-                                        System.out.format("Unable to find file '%s'\n", fileName);
+                                        logger.log(DEFAULT_LOG_LEVEL, "Unable to find file '{0}'\n", fileName);
                                     } catch (IOException e) {
-                                        System.out.format("Unable to write to file '%s'\n", fileName);
+                                        logger.log(DEFAULT_LOG_LEVEL, "Unable to write to file '{0}'\n", fileName);
                                     } catch (Exception e) {
                                         e.printStackTrace();
                                     }
                                     break;
                                 default:
-                                    System.out.printf("Please supply a destination filename.\n");
+                                    logger.log(DEFAULT_LOG_LEVEL, "Please supply a destination filename.\n");
                             }
                             break;
                         case "--llaf":
@@ -85,27 +90,14 @@ public class AuctionServer {
                         case "--load-love-auctions-from-file":
                             switch (input.length) {
                                 case 2:
-                                    String fileName = input[1];
-                                    File f = new File(fileName);
-                                    try(FileReader fr = new FileReader(f); BufferedReader bfr = new BufferedReader(fr)){
-                                        String line;
-                                        List<String[]> result = new ArrayList<>();
-                                        while((line = bfr.readLine()) != null) {
-                                            String[] inputs = line.split(" ");
-                                            result.add(inputs);
-                                        }
-                                        auction.bulkCreateAndRegisterAuctionItems(-1111, result);
-                                    } catch (NumberFormatException e){
-                                        e.printStackTrace();
-                                        auction.rollBackLastBulkAdd(this.id);
-                                    }
+                                    readAuctionItemsFromFile(input[1]);
                                     break;
                                 default:
-                                    System.out.printf("Please supply a source filename.\n");
+                                    logger.log(DEFAULT_LOG_LEVEL, "Please supply a destination filename.\n");
                             }
                             break;
                         default:
-                            System.out.format("Unrecognizable command :%s\n", input[0]);
+                            logger.log(DEFAULT_LOG_LEVEL, "Unrecognizable command :{0}\n", input[0]);
                     }
                 }
             } catch (Exception e) {
@@ -114,7 +106,30 @@ public class AuctionServer {
         }
     }
 
+    private void readAuctionItemsFromFile(String fileName) throws AuctionException {
+        File f = new File(fileName);
+        try (FileReader fr = new FileReader(f); BufferedReader bfr = new BufferedReader(fr)) {
+            String line;
+            List<String[]> result = new ArrayList<>();
+            while ((line = bfr.readLine()) != null) {
+                String[] inputs = line.split(" ");
+                result.add(inputs);
+            }
+            auction.bulkCreateAndRegisterAuctionItems(-1111, result);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            auction.rollBackLastBulkAdd(this.id);
+        } catch (FileNotFoundException e) {
+            logger.log(DEFAULT_LOG_LEVEL, "Unable to find file '{0}'\n", f.getName());
+        } catch (IOException e) {
+            logger.log(DEFAULT_LOG_LEVEL, "Unable to write to file '{0}'\n", f.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        AuctionServer as = new AuctionServer(args[0]);
+        new AuctionServer(args);
+
     }
 }
